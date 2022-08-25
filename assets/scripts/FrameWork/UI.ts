f7a8adf6ab970d88cb5fa44816a723b6e958c42f
375069737@qq.com <375069737@qq.com>
2022-08-25 21:11:46 +0800

    初步版本

--- assets/scripts/FrameWork/UI.ts
+++
@@ -1,9 +0,0 @@
-import Global from "./Global";
-
-export default class UI {
-
-    public showLockUI(isShow: boolean) {
-        let global: cc.Node = cc.director.getScene().getChildByName('Global');
-        global.getComponent(Global).isShowLockUI(isShow);
-    }
-}
--- assets/scripts/gameMain/RotateSelf.ts
+++
@@ -1,8 +0,0 @@
-const { ccclass, property } = cc._decorator;
-
-@ccclass
-export default class RotateSelf extends cc.Component {
-    start() {
-        cc.tween(this.node).by(2, { angle: -360 }).repeatForever().start();
-    }
-}
